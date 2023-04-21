951f6756b71b01b7c104430bb7f0d2700d091211
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-21 13:43:15 +0300

    first view

--- iss_app/views.py
+++ iss_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "iss_app/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
