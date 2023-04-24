4ff624f466490af92e64c62e40a4f98c7b9c555f
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-24 16:24:04 +0300

    iss parameters table

--- calculations/iss/iss_params.py
+++ calculations/iss/iss_params.py
@@ -1,26 +1,26 @@
 import requests
 from calculations import unix_time_converter
 
-
 ISS_API = "https://api.wheretheiss.at/v1/satellites/25544"
 
 
 def iss_data():
     req = requests.get(ISS_API).json()
+
     data = {
 
         'lat': req["latitude"],
         'lon': req["longitude"],
         'alt': req["altitude"],
         'vel_kph': int(req["velocity"]),
-        'vel_mps': round((int(req["velocity"])/3.6), 2),
+        'vel_mps': round((int(req["velocity"]) / 3.6), 2),
         'vis': req["visibility"],
         'daynum': req["daynum"],
+
     }
 
     return data
 
 
 if __name__ == "__main__":
-
     print(iss_data())
--- iss_app/views.py
+++ iss_app/views.py
@@ -2,21 +2,41 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 import folium
+import requests
+import geocoder
 
 from calculations.iss import iss_params, people_on_board
 
 
 def home_view(request):
-    return render(request, "iss_app/index.html", {})
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    return render(request, "iss_app/index.html", {'ip': ip})
 
 
 @require_GET
 def map_view(request):
-    if 'refresh' in request.GET:
+    data = iss_params.iss_data()
+    lat = float(data['lat'])
+    lon = float(data['lon'])
 
-        data = iss_params.iss_data()
-        lat = float(data['lat'])
-        lon = float(data['lon'])
+    table_data = {
+        'lat': round(data['lat'], 2),
+        'lon': round(data['lon'], 2),
+        'alt': round(data['alt'], 2),
+        'vel_kph': data['vel_kph'],
+        'vel_mps': data['vel_mps'],
+        'vis': data['vis'],
+        'pob': people_on_board.people_iss()['people']
+    }
+
+    user_ip = ''
+
+    if 'refresh' in request.GET:
 
         updating_map = folium.Map(location=[lat, lon], zoom_start=3)
 
@@ -27,15 +47,12 @@ def map_view(request):
 
         context = {
             'map': updating_map._repr_html_(),
+            'data': table_data
         }
 
         return JsonResponse(context)
 
     else:
-
-        data = iss_params.iss_data()
-        lat = float(data['lat'])
-        lon = float(data['lon'])
 
         initial_map = folium.Map(location=[lat, lon], zoom_start=3)
 
@@ -45,7 +62,8 @@ def map_view(request):
         # folium.CircleMarker(location=(data['lat'], data['lon']), radius=30, fill_color='gray').add_to(initial_map)
 
         context = {
-            'map': initial_map._repr_html_()
+            'map': initial_map._repr_html_(),
+            'data': table_data
         }
 
         return render(request, 'iss_app/map.html', context)
